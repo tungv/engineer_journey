6ab6785768d6d74faab76db4435a1cd0ebe29611
Tung <tung@bodidata.com>
2020-01-01 21:38:46 +0700

    feat: basic client

---
+++ ep4/client/src/Layout.js
@@ -0,0 +1,14 @@
+export default function Layout(props) {
+  return (
+    <main>
+      <style global jsx>{`
+        body,
+        html {
+          margin: 0;
+          padding: 0;
+        }
+      `}</style>
+      {props.children}
+    </main>
+  );
+}
---
+++ ep4/client/src/pages/user/[id].js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import Layout from "../../Layout";
+
+export default function UserPage(props) {
+  return <Layout>...</Layout>;
+}
