17920547002970e8c8b07f14f6327f708f9baf65
Tung <tung@bodidata.com>
2020-01-01 21:43:45 +0700

    chore: setup apollo

--- ep4/client/src/Layout.js
+++ ep4/client/src/Layout.js
@@ -1,14 +1,19 @@
+import { ApolloProvider } from "@apollo/react-hooks";
+import client from "./apollo-client";
+
 export default function Layout(props) {
   return (
-    <main>
-      <style global jsx>{`
-        body,
-        html {
-          margin: 0;
-          padding: 0;
-        }
-      `}</style>
-      {props.children}
-    </main>
+    <ApolloProvider client={client}>
+      <main>
+        <style global jsx>{`
+          body,
+          html {
+            margin: 0;
+            padding: 0;
+          }
+        `}</style>
+        {props.children}
+      </main>
+    </ApolloProvider>
   );
 }
